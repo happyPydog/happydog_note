673c4e4e4e6d49760e3e64bc05eaa3d4ea1dfcd8
Rammstein-1994 <lanc840930.mg09@nycu.edu.tw>
2024-03-16 16:35:07 +0800

    add python doc creating

---
+++ python_note/python_doc/my_function.py
@@ -0,0 +1,12 @@
+class A:
+    """
+    Description of class A.
+
+    Parameters:
+        a (int): Description of a.
+        b (str): Description of b.
+    """
+
+    def __init__(self, a: int, b: str) -> None:
+        self.a = a
+        self.b = b
